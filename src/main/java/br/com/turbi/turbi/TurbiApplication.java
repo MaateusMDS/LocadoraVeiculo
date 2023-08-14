c4284270af30e9bf88b37fb10f11b5dc8321291c
MaateusMDS <maateusmania@gmail.com>
2023-08-14 20:06:02 -0300

    starting project

---
+++ src/main/java/br/com/turbi/turbi/TurbiApplication.java
@@ -0,0 +1,13 @@
+package br.com.turbi.turbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/turbi/turbi/TurbiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.turbi.turbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
