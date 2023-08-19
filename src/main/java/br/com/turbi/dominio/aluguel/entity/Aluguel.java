96c5600395f2ad94e0bbccdc0fe3a51ab5c45070
MaateusMDS <maateusmania@gmail.com>
2023-08-18 21:47:28 -0300

    Aluguel entity

--- src/main/java/br/com/turbi/dominio/aluguel/entity/Aluguel.java
+++ src/main/java/br/com/turbi/dominio/aluguel/entity/Aluguel.java
@@ -2,6 +2,7 @@ package br.com.turbi.dominio.aluguel.entity;
 
 import br.com.turbi.dominio.cliente.entity.Cliente;
 import br.com.turbi.dominio.veiculo.entity.Veiculo;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -9,17 +10,43 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(exclude = {"cliente", "veiculo", "dataInicio", "dataTermino", "valorTotal"})
+
+@Entity
+@Table(name = "TB_TURBI_ALUGUEL")
 public class Aluguel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_TURBI_ALUGUEL")
+	@SequenceGenerator(name = "SQ_TURBI_ALUGUEL", sequenceName = "SQ_TURBI_ALUGUEL", allocationSize = 1)
 	private long id;
 	@Getter @Setter
+	@ManyToOne(
+			fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+	)
+	@JoinColumn(
+			name = "ID_CLIENTE",
+			referencedColumnName = "ID_CLIENTE",
+			foreignKey = @ForeignKey(name = "FK_CLIENTE_ALUGUEL")
+	)
 	private Cliente cliente;
 	@Getter @Setter
+	@ManyToOne(
+			fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+	)
+	@JoinColumn(
+			name = "ID_VEICULO",
+			referencedColumnName = "ID_VEICULO",
+			foreignKey = @ForeignKey(name = "FK_VEICULO_ALUGUEL")
+	)
 	private Veiculo veiculo;
 	@Getter @Setter
+	@Column(name = "DT_INICIO")
 	private LocalDate dataInicio;
 	@Getter @Setter
+	@Column(name = "DT_TERMINO")
 	private LocalDate dataTermino;
 	@Getter @Setter
+	@Column(name = "VL_TOTAL")
 	private BigDecimal valorTotal;
 
 	public BigDecimal calcularValorAluguel() {
